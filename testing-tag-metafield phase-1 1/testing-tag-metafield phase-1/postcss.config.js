3537536c8b6ea62a2579d7735b3b2c4c0043d0cd
Adarsh Gautam <109784927+adarshgautam101@users.noreply.github.com>
2026-01-12 13:25:22 +0530

    Delete testing-tag-metafield phase-1 1/testing-tag-metafield phase-1 directory

--- testing-tag-metafield phase-1 1/testing-tag-metafield phase-1/postcss.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    plugins: {
-        "@tailwindcss/postcss": {},
-        autoprefixer: {}, // autoprefixer is usually included in @tailwindcss/postcss but safely kept if explicit
-    },
-};
